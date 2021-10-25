230b01943ff942157f0acde011c0896904f4f48a
ianzuresgit config --global user.name ianzures <i.anzures514@gmail.com>
2021-10-24 23:25:05 -0400

    Search implemented. Changed handler to volley.

--- app/src/main/java/com/example/newssolo/MainActivity.java
+++ app/src/main/java/com/example/newssolo/MainActivity.java
@@ -7,8 +7,22 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
@@ -23,50 +37,79 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+//import kotlin.sequences.TakeWhileSequence;
 import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "MainActivity";
 
+    public static final String TAG = "MainActivity";
+    EditText tvSearch;
+    RecyclerView rvArticles;
     List<Article> articles;
+    ArticleAdapter articleAdapter;
+    AsyncHttpClient client;
+    int numItems = 20;
+
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Remove title bar
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        RecyclerView rvArticles = findViewById(R.id.rvArticles);
+
+        tvSearch = (EditText) findViewById(R.id.tvSearch);
+        rvArticles = findViewById(R.id.rvArticles);
 
         articles = new ArrayList<>();
+        articleAdapter = new ArticleAdapter(this, articles);
 
-        ArticleAdapter articleAdapter = new ArticleAdapter(this, articles);
         rvArticles.setAdapter(articleAdapter);
-
         rvArticles.setLayoutManager(new LinearLayoutManager(this));
 
-        int numItems = 3;
-        String tickers = "FB";
+        requestQueue = Volley.newRequestQueue(this);
 
-        String url = String.format("https://stocknewsapi.com/api/v1?tickers=%s,&items=%d&token=i0rpdgcnbrcgaimxbclxhztmuu6sk8jm79zcludj&fbclid=IwAR0pguARasu-pDs_Jcy4Wc4fCL_JIXCjRc_JYwsSN57xOSCnhleL3I2LDHA", tickers,numItems);
-
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(url, new JsonHttpResponseHandler() {
+        tvSearch.setOnKeyListener(new View.OnKeyListener(){
             @Override
-            public void onSuccess(int statusCode, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess");
-                JSONObject jsonObject = json.jsonObject;
-                try {
-                    JSONArray results = jsonObject.getJSONArray("data");// results is an array in the json
-                    articles.addAll(Article.fromJsonArray(results));
-                    articleAdapter.notifyDataSetChanged();
-                } catch (JSONException e) {
-                    Log.e(TAG, "Hit json exception", e);
-                    e.printStackTrace();
+            public boolean onKey(View view, int i , KeyEvent keyEvent){
+                // If the event is a key-down event on the "enter" button
+                if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (i == 66)) {
+                    jsonParse();
+                    return true;
                 }
-            }
-            @Override
-            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.d(TAG, "onFailure");
+                return false;
             }
         });
     }
+
+    private void jsonParse(){
+        String tickers = tvSearch.getText().toString();;
+        String url = String.format("https://stocknewsapi.com/api/v1?tickers=%s,&items=%d&token=i0rpdgcnbrcgaimxbclxhztmuu6sk8jm79zcludj&fbclid=IwAR0pguARasu-pDs_Jcy4Wc4fCL_JIXCjRc_JYwsSN57xOSCnhleL3I2LDHA",tickers,numItems);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray results = response.getJSONArray("data");// results is an array in the json
+                            articles.addAll(Article.fromJsonArray(results));
+                            articleAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(request);
+    }
 }
+
+
